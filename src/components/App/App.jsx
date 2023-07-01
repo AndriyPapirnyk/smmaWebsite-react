cd2da7346ea0e9b8beea6bf8707f83bcfc6e575d
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-07-01 17:35:31 +0300

    adding gulpfile and making folder scructure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from '../Home/Home';
+
+function App() {
+  return (
+    <div className="app">
+      <div className='app__container'>
+        <Home/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import './Nav.css';
+
+function Nav() {
+  return (
+    <nav className='nav'>
+        
+    </nav>
+  );
+}
+
+export default Nav;
