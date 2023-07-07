bfe64c26db91c3414a2c8cff76609a7e0774914f
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-07-08 00:47:19 +0300

    design correctons

---
+++ src/components/Services/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,17 @@
+import './ServiceCard.css';
+
+function ServiceCard() {
+  return (
+    <div className="serviceCard">
+        <div className="serviceCard__icon">
+            <img src="" alt="" />
+        </div>
+        <div className="serviceCard__main">
+            <h1></h1>
+            <p></p>
+        </div>
+    </div>
+  );
+}
+
+export default ServiceCard;
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,7 +4,10 @@ function Service() {
   return (
     <section className="service">
       <div className="service__container">
-
+        <h1 className='service__heading'><span>O</span>ur Service</h1>
+        <div className="service__main">
+          
+        </div>
       </div>
     </section>
   );
