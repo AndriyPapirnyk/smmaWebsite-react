01a5984637f6297fc494485dd36f5bc82c6d8b39
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-07-08 02:30:44 +0300

    service block

--- src/components/Services/ServiceCard/ServiceCard.jsx
+++ src/components/Services/ServiceCard/ServiceCard.jsx
@@ -1,14 +1,23 @@
 import './ServiceCard.css';
+import arrowImg from './img/arrow.png'
 
-function ServiceCard() {
+function ServiceCard(props) {
+
+//   const { cardWidth } = props.width;
+  
+
   return (
-    <div className="serviceCard">
+    <div className="serviceCard" style={{width: `${props.width}`}}>
+        <h1 className='serviceCard__heading'>{props.heading}</h1>
         <div className="serviceCard__icon">
-            <img src="" alt="" />
+            <img src={props.icon} alt="" />
         </div>
-        <div className="serviceCard__main">
-            <h1></h1>
-            <p></p>
+        {/* <div className="serviceCard__main">
+            <h1>{props.heading}</h1>
+            <p>{props.desciption}</p>
+        </div> */}
+        <div className='serviceCard__button'>
+            <img src={arrowImg} alt="" />
         </div>
     </div>
   );
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,12 +1,63 @@
 import './Services.css';
+import ServiceCard from './ServiceCard/ServiceCard';
+import smmIcon from './img/smm.png'
+import tiktokIcon from './img/tiktok.png';
+import instagramIcon from './img/instagram.png';
+import youtubeIcon from './img/youtube.png';
+import facebookIcon from './img/facebook.png';
+import amazonIcon from './img/amazon.png';
+
+
+
+
+
+
+const serviceList = [
+  {
+    width: '49.5%',
+    heading: '01. SMM',
+    desciption: 'Lorem  ',
+    icon: smmIcon
+  },
+  {
+    width: '49.5%',
+    heading: '02. TikTok',
+    desciption: 'Lorem',
+    icon: tiktokIcon
+  },
+  {
+    width: '49.5%',
+    heading: '03. Instagram',
+    desciption: 'Lorem',
+    icon: instagramIcon
+  },
+  {
+    width: '49.5%',
+    heading: '04. YouTube ',
+    desciption: 'Lorem',
+    icon: youtubeIcon
+  },
+  {
+    width: '49.5%',
+    heading: '05. Facebook',
+    desciption: 'Lorem',
+    icon: facebookIcon
+  },
+  {
+    width: '49.5%',
+    heading: '06. Amazon',
+    desciption: 'Lorem',
+    icon: amazonIcon
+  },
+]
 
 function Service() {
   return (
     <section className="service">
       <div className="service__container">
-        <h1 className='service__heading'><span>O</span>ur Service</h1>
+        <h1 className='service__heading'><span>O</span>ur <span>Service</span></h1>
         <div className="service__main">
-          
+          {serviceList.map(item => <ServiceCard key={item.heading} width={item.width} heading={item.heading} desciption={item.desciption} icon={item.icon} />)}
         </div>
       </div>
     </section>
