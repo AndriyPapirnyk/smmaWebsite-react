4a0abb72e22fe85d6233526c5cbda443175fea8e
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-07-11 18:06:52 +0300

    subscription block

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import '../node_modules/reset-css/reset.css';
 import Home from './components/Home/Home';
 import Service from './components/Services/Services';
+import Subscription from './components/Subscription/Subscription';
 
 function App() {
   return (
     <div className="app">
       <Home/>
       <Service/>
+      <Subscription />
     </div>
   );
 }
