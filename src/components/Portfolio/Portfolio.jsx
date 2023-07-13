3748993aa41e12ec42202ed2ff7f81ece065ad5f
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-07-13 14:14:35 +0300

    adding swiper

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,12 +1,12 @@
 import './Portfolio.css';
+import PortfolioSlider from './PortfolioSlider/PortfolioSlider';
 
 function Portfolio() {
   return (
     <section className="portfolio">
       <div className="portfolio__container">
         <h1><span>O</span>ur <span>Portfolio</span></h1>
-
-        <div></div>
+        <PortfolioSlider/>
       </div>
     </section>
   );
